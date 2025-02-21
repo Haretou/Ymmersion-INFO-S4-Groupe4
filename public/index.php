f5ab8b94745a1873db2d672fb510d93a46ff479b
Raf_lgo <raphael.langoisco@ynov.com>
2025-02-21 10:56:03 +0100

    index update

--- public/index.php
+++ public/index.php
@@ -63,7 +63,7 @@ $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <title>Accueil - E-Commerce</title>
 </head>
 <body>
